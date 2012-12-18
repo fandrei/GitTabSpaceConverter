bd1a47ffbd3f9fdf5fc0e4103273c5f2d8f29de0
fandrei <andrei_f_11@mail.ru>
2012-12-18 15:13:00 +0700

    bugfix

--- GitSetup.bat
+++ GitSetup.bat
@@ -1,2 +1,2 @@
-call git config --global filter.TabsVsSpaces.smudge '%~dp0\TabSpaceConverter\bin\Debug\TabSpaceConverter.exe' MakeSpaces
-call git config --global filter.TabsVsSpaces.clean '%~dp0\TabSpaceConverter\bin\Debug\TabSpaceConverter.exe' MakeTabs
+call git config --global --replace-all filter.TabsVsSpaces.smudge "'%~dp0\TabSpaceConverter\bin\Debug\TabSpaceConverter.exe' MakeSpaces"
+call git config --global --replace-all filter.TabsVsSpaces.clean "'%~dp0\TabSpaceConverter\bin\Debug\TabSpaceConverter.exe' MakeTabs"
